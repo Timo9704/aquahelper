0914d64f87ed489b10f5ee42051d7873c86e0fd2
Timo Schmitz <Timo.schmitz1@web.de>
2023-11-13 19:36:51 +0100

    Adjust Aquarium-model for SQLite

--- lib/model/aquarium.dart
+++ lib/model/aquarium.dart
@@ -1,22 +1,31 @@
 class Aquarium {
-  int? id;
-  String? name;
-  int? liter;
-  String? imageUrl;
+  int id;
+  String name;
+  int liter;
+  String imageUrl;
 
-  Aquarium({
+  Aquarium(
     this.id,
     this.name,
     this.liter,
-    this.imageUrl
-  });
+    this.imageUrl,
+  );
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'liter': liter,
-      'imageUrl': imageUrl,
-    };
+  factory Aquarium.fromMap(Map<String, dynamic> json){
+    return Aquarium(
+        json["id"],
+        json["name"],
+        json["liter"],
+        json["imageUrl"]
+    );
   }
-}
+
+    Map<String, dynamic> toMap() {
+      return {
+        'id': id,
+        'name': name,
+        'liter': liter,
+        'imageUrl': imageUrl,
+      };
+    }
+  }
