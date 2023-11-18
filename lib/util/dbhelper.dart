6bb590780cfb55e93d48d6d9baf2d74d9fa0fe57
Timo Schmitz <Timo.schmitz1@web.de>
2023-11-18 20:05:41 +0100

    Edit dbHelper and create test data

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -11,8 +11,8 @@ class DBHelper {
     String path = join(await getDatabasesPath(), 'aquarium_database.db');
     return await openDatabase(path, version: 1, onOpen: (db) {},
         onCreate: (Database db, int version) async {
-          await db.execute('CREATE TABLE tank(id INTEGER PRIMARY KEY, name TEXT, liter INTEGER, imageUrl TEXT)');
-          await db.execute('CREATE TABLE measurement(id INTEGER PRIMARY KEY, temp FLOAT, ph FLOAT, gh FLOAT, kh FLOAT, no2 FLOAT, no3 FLOAT, po4 FLOAT, k FLOAT, mg FLOAT)');
+          await db.execute('CREATE TABLE tank(aquariumId INTEGER PRIMARY KEY, name TEXT, liter INTEGER, imageUrl TEXT)');
+          await db.execute('CREATE TABLE measurement(measurementId INTEGER PRIMARY KEY, aquariumId INTEGER, temp FLOAT, ph FLOAT, gh FLOAT, kh FLOAT, no2 FLOAT, no3 FLOAT, po4 FLOAT, k FLOAT, mg FLOAT, FOREIGN KEY(aquariumId) REFERENCES tank(aquariumId))');
         });
   }
 
@@ -22,18 +22,30 @@ class DBHelper {
     List<Aquarium> list = res.isNotEmpty ? res.map((c) => Aquarium.fromMap(c)).toList() : [];
     return list;
   }
+
   // Define a function that inserts dogs into the database
-  Future<void> insertAquarium() async {
+  Future<void> insertAquarium(Aquarium aquarium) async {
+    final db = await openDatabase('aquarium_database.db');
+
+    await db.insert(
+      'tank',
+      aquarium.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // Define a function that inserts dogs into the database
+  Future<void> insertAquarien() async {
     final db = await openDatabase('aquarium_database.db');
 
     Map<String, dynamic> aquariumMap = {
-      "id": 1,
+      "aquariumId": 1,
       "name": "Alpen 60P",
       "liter": 80,
       "imageUrl": "https://aquaristik-kosmos.de/wp-content/uploads/2022/12/Aquarium_weboptimiert_720p_low.jpg"
     };
     Map<String, dynamic> aquariumMap1 = {
-      "id": 2,
+      "aquariumId": 2,
       "name": "Dragon Mini M",
       "liter": 20,
       "imageUrl": "https://aquaristik-kosmos.de/wp-content/uploads/2022/12/Aquarium_weboptimiert_720p_low.jpg"
