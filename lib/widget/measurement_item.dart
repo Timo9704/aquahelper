3448dcb874866008c4090844b1f0c61b57044d47
Timo Schmitz <Timo.schmitz1@web.de>
2023-11-28 10:18:08 +0100

    Clean up measurement_item class

--- lib/widget/measurement_item.dart
+++ lib/widget/measurement_item.dart
@@ -1,32 +1,43 @@
-import 'package:aquahelper/screens/measurement_form.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../model/aquarium.dart';
 import '../model/measurement.dart';
+import 'package:aquahelper/screens/measurement_form.dart';
 
-class MeasurementItem extends StatelessWidget{
+class MeasurementItem extends StatelessWidget {
   final Measurement measurement;
   final Aquarium aquarium;
-  MeasurementItem({super.key, required this.measurement, required this.aquarium});
+
+  MeasurementItem(
+      {super.key, required this.measurement, required this.aquarium});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-        elevation: MaterialStateProperty.all(10.0)
-      ),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          elevation: MaterialStateProperty.all(10.0)),
       onPressed: () {
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => MeasurementForm(measurementId: measurement.measurementId, aquarium: aquarium)),
+          MaterialPageRoute(
+              builder: (context) => MeasurementForm(
+                  measurementId: measurement.measurementId,
+                  aquarium: aquarium)
+          ),
         );
       },
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text('Messung vom', style: const TextStyle(fontSize: 18, color: Colors.black)),
-          Text(DateFormat('dd.MM.yyyy').format(DateTime.fromMillisecondsSinceEpoch(measurement.measurementDate)), style: TextStyle(fontSize: 18, color: Colors.black)),
+          Text('Messung vom',
+              style: const TextStyle(fontSize: 18, color: Colors.black)),
+          Text(
+              DateFormat('dd.MM.yyyy').format(
+                  DateTime.fromMillisecondsSinceEpoch(
+                      measurement.measurementDate)),
+              style: TextStyle(fontSize: 18, color: Colors.black)
+          ),
         ],
       ),
     );
