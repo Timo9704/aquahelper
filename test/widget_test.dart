1ac7346c5892f8c0865047fe279f50b393885784
Timo Schmitz <Timo.schmitz1@web.de>
2023-12-11 21:05:58 +0100

    Add tests and sqlite dev dependency

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,42 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:aquahelper/main.dart';
+import 'package:aquahelper/model/aquarium.dart';
+import 'package:aquahelper/util/dbhelper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:uuid/uuid.dart';
 
+/// Initialize sqflite for test.
+void sqfliteTestInit() {
+  // Initialize ffi implementation
+  sqfliteFfiInit();
+  // Set global factory
+  databaseFactory = databaseFactoryFfi;
+}
 
-void main() {
+Future main() async {
+  sqfliteTestInit();
+  await DBHelper.db.initDB();
+
+  testWidgets('Startscreen is empty and shows adding statement', (tester) async {
+
+    await tester.pumpWidget(const AquaHelper());
+
+    final titleFinder = find.text('Lege dein erstes Aquarium an!');
+
+    expect(titleFinder, findsOneWidget);
+  });
+
+  testWidgets('Insert aquarium', (tester) async {
+
+    print('M;mm');
+
+    await tester.pumpWidget(const AquaHelperStartPage());
+
+    final titleFinder = find.text('Alpen 60P');
+
+    expect(titleFinder, findsOneWidget);
+  });
 }
