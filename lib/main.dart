9341e6e3412d6910c7370993fbdfdd6e7415bfe3
Timo Schmitz <Timo.schmitz1@web.de>
2023-12-21 17:55:39 +0100

    Fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:aquahelper/screens/create_or_edit_aquarium.dart';
 import 'package:aquahelper/screens/homepage.dart';
+import 'package:aquahelper/screens/infopage.dart';
+import 'package:aquahelper/widget/aquarium_item.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:aquahelper/util/dbhelper.dart';
+
+import 'model/aquarium.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -134,7 +139,6 @@ class _AquaHelperStartPageState extends State<AquaHelperStartPage> {
           )
         ],
       ),
->>>>>>> main
     );
   }
 }
