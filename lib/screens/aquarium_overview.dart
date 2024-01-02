1606dbf78d21ebdb8e7f0d561f899af02371a6ff
Timo Schmitz <Timo.schmitz1@web.de>
2024-01-02 16:28:05 +0100

    Commit

--- lib/screens/aquarium_overview.dart
+++ lib/screens/aquarium_overview.dart
@@ -1,4 +1,8 @@
 import 'dart:io';
+import 'package:aquahelper/util/notification_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter/material.dart';
 
 import 'package:aquahelper/model/aquarium.dart';
@@ -8,6 +12,8 @@ import 'package:aquahelper/screens/infopage.dart';
 import 'package:aquahelper/screens/measurement_form.dart';
 import 'package:aquahelper/screens/chart_analysis.dart';
 import 'package:aquahelper/util/dbhelper.dart';
+
+import '../widget/reminder_item.dart';
 
 class AquariumOverview extends StatefulWidget {
   final Aquarium aquarium;
@@ -20,6 +26,11 @@ class AquariumOverview extends StatefulWidget {
 
 class AquariumOverviewState extends State<AquariumOverview> {
   List<Measurement> measurementList = [];
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  bool _notificationsEnabled = false;
+
 
   @override
   void initState() {
@@ -33,6 +44,22 @@ class AquariumOverviewState extends State<AquariumOverview> {
     setState(() {
       measurementList = dbMeasurements.reversed.toList();
     });
+  }
+
+  Future<void> areNotifcationsEnabledOnAndroid() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+        const NotificationDetails(
+            android: AndroidNotificationDetails(
+                '0', // channel Id
+                'general' // channel Name
+            )),
+        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+        uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime);
   }
 
   @override
@@ -71,6 +98,53 @@ class AquariumOverviewState extends State<AquariumOverview> {
                     fit: BoxFit.cover)),
           ),
           Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Alle Erinnerungen:',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w800)),
+                IconButton(
+                  onPressed: () async {
+                    PermissionStatus status = await Permission.notification.request();
+                    if (status.isGranted) {
+                      NotificationService().zonedSchedule();
+                      final List<PendingNotificationRequest> pendingNotificationRequests =
+                      await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+                      for(int i = 0; i<pendingNotificationRequests.length; i++){
+                        print(pendingNotificationRequests.elementAt(i).title);
+                      }
+                    }
+                    else {
+                      // Open settings to enable notification permission
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.lightGreen,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 5,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              itemBuilder: (context, index) {
+                return const ReminderItem(
+                    name: "Wasserwechsel",
+                    dueDay: 2);
+              },
+            ),
+          ),
+          Padding(
             padding: const EdgeInsets.all(10.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -98,7 +172,7 @@ class AquariumOverviewState extends State<AquariumOverview> {
           ),
           Container(
             padding: const EdgeInsets.all(5.0),
-            height: MediaQuery.of(context).size.height * 0.45,
+            height: MediaQuery.of(context).size.height * 0.25,
             child: ListView.builder(
               scrollDirection: Axis.vertical,
               itemCount: measurementList.length,
