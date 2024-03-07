510c99f897d5a44e25e0b493c55ccad07ef2e80a
Timo Schmitz <Timo.schmitz1@web.de>
2024-03-07 13:44:59 +0100

    Fix lint

--- lib/screens/settings/feedback_form.dart
+++ lib/screens/settings/feedback_form.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
-import '../../main.dart';
 import '../../util/scalesize.dart';
 import '../homepage.dart';
 
