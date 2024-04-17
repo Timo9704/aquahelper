2ce6a52297488189b5716909762c3ddbaa77b8d2
Timo Schmitz <Timo.schmitz1@web.de>
2024-04-17 07:06:31 +0200

    Fix lint

--- lib/screens/settings/feedback_form.dart
+++ lib/screens/settings/feedback_form.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:aquahelper/config.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
