fc7ade244ed80d2b8a6216f59df90720b056dce7
Timo Schmitz <Timo.schmitz1@web.de>
2024-07-27 17:58:51 +0200

    Save current result page

--- lib/screens/aifeatures/aiplanner/ai_planner_result.dart
+++ lib/screens/aifeatures/aiplanner/ai_planner_result.dart
@@ -1,114 +1,23 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'ai_planner_guide.dart';
-class AiPlannerResult extends StatefulWidget {
 
-  const AiPlannerResult({super.key});
+class AiPlannerResult extends StatefulWidget {
+  final Map<String, dynamic> jsonData;
+  final int planningMode;
+  const AiPlannerResult({super.key, required this.jsonData, required this.planningMode});
 
   @override
   State<AiPlannerResult> createState() => _AiPlannerResultState();
 }
 
 class _AiPlannerResultState extends State<AiPlannerResult> {
+  double textScaleFactor = 0;
 
   @override
   void initState() {
     super.initState();
   }
-
-  final jsonData = '''
-  {
-    "aquarium": {
-      "aquarium_name" : "Juwel Rio 125",
-      "aquarium_liter" : "180",
-      "aquarium_price" : "299.99",
-      "aquarium_technic" : ["Filter", "Heizer"]
-    },
-    "special_technic": {
-      "filter" : "eingebaut",
-      "light" : "Chihiros WRGB2",
-      "co2" : "Dennerle ProFlora 360"
-    },
-    "plants": {
-      "foreground" : [
-          {
-              "name" : "Eleocharis acicularis",
-              "amount" : "5",
-              "price" : "2.99"
-          },
-          {
-              "name" : "Lilaeopsis brasiliensis",
-              "amount" : "3",
-              "price" : "3.99"
-          }
-      ],
-      "midground" : [
-          {
-              "name" : "Cryptocoryne wendtii",
-              "amount" : "2",
-              "price" : "4.99"
-          },
-          {
-              "name" : "Hygrophila pinnatifida",
-              "amount" : "1",
-              "price" : "5.99"
-          }
-      ],
-      "background" : [
-          {
-              "name" : "Echinodorus bleheri",
-              "amount" : "1",
-              "price" : "6.99"
-          },
-          {
-              "name" : "Vallisneria spiralis",
-              "amount" : "3",
-              "price" : "7.99"
-          }
-      ]
-    },
-    "animals": {
-      "fishes" : [
-          {
-              "name" : "Neons",
-              "amount" : "10",
-              "price" : "1.99"
-          },
-          {
-              "name" : "Guppys",
-              "amount" : "5",
-              "price" : "2.99"
-          }
-      ],
-      "shrimps" : [
-          {
-              "name" : "Red Fire",
-              "amount" : "5",
-              "price" : "3.99"
-          },
-          {
-              "name" : "Amano",
-              "amount" : "3",
-              "price" : "4.99"
-          }
-      ],
-      "snails" : [
-          {
-              "name" : "Posthorn",
-              "amount" : "2",
-              "price" : "5.99"
-          },
-          {
-              "name" : "Turmdeckelschnecke",
-              "amount" : "1",
-              "price" : "6.99"
-          }
-      ]
-    }
-  }
-  ''';
 
   void handleClick(String value) {
     switch (value) {
@@ -121,79 +30,190 @@ class _AiPlannerResultState extends State<AiPlannerResult> {
 
   @override
   Widget build(BuildContext context) {
-    final data = jsonDecode(jsonData);
+    final Map<String, dynamic> data = widget.jsonData;
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("KI-Planer - Bepflanzung"),
-          backgroundColor: Colors.lightGreen,
-          actions: <Widget>[
-            PopupMenuButton<String>(
-              onSelected: handleClick,
-              itemBuilder: (BuildContext context) {
-                return {'Anleitung'}.map((String choice) {
-                  return PopupMenuItem<String>(
-                    value: choice,
-                    child: Text(choice),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-        ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-        children: [
-          buildSection('Aquarium & Technik', data['aquarium']),
-          buildSection('Zubehör', data['special_technic']),
-          buildSection('Besatz', data['animals']),
-          buildSection('Bepflanzung', data['plants']),
+      appBar: AppBar(
+        title: const Text("KI-Planer"),
+        backgroundColor: Colors.lightGreen,
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: handleClick,
+            itemBuilder: (BuildContext context) {
+              return {'Anleitung'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
         ],
       ),
-      )
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text("KI-Planer",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w800)),
+            const Text("Planungsergebnis",
+                style: TextStyle(fontSize: 20, color: Colors.black)),
+            const SizedBox(height: 10),
+            if(widget.planningMode == 0)
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: DataTable(
+                  columnSpacing: 10,
+                  dataRowMaxHeight: 50,
+                  columns: [
+                    DataColumn(
+                      label: const SizedBox(
+                        width: 130, // gleiche Breite für alle Spalten
+                        child: Text(''),
+                      ),
+                      onSort: (int columnIndex, bool ascending) => {},
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 160, // gleiche Breite für alle Spalten
+                        child: Text('Name'),
+                      ),
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 50, // gleiche Breite für alle Spalten
+                        child: Text(''),
+                      ),
+                    ),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      const DataCell(Text('Aquarium-Modell')),
+                      DataCell(Text(data['aquarium']['aquarium_name'])),
+                      DataCell(LinkWidget(data['aquarium']['link'])),
+                    ]),
+                    DataRow(cells: [
+                      const DataCell(Text('Preis')),
+                      DataCell(Text(data['aquarium']['aquarium_price'])),
+                      DataCell(SizedBox.shrink()),
+                    ]),
+                    DataRow(cells: [
+                      const DataCell(Text('Bestandteile')),
+                      DataCell(
+                          Text(data['aquarium']['included_items'].join(', '))),
+                      DataCell(SizedBox.shrink()),
+                    ]),
+                  ],
+                ),
+              ),
+            if(widget.planningMode == 0 || widget.planningMode == 1)
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: DataTable(
+                  columnSpacing: 10,
+                  dataRowMaxHeight: 50,
+                  columns: [
+                    DataColumn(
+                      label: const SizedBox(
+                        width: 130, // gleiche Breite für alle Spalten
+                        child: Text('Fischart'),
+                      ),
+                      onSort: (int columnIndex, bool ascending) => {},
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 160, // gleiche Breite für alle Spalten
+                        child: Text('Menge'),
+                      ),
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 50, // gleiche Breite für alle Spalten
+                        child: Text(''),
+                      ),
+                    ),
+                  ],
+                  rows: data['fish']
+                      .map<DataRow>((fish) => DataRow(cells: [
+                            DataCell(Text(fish['fish_common_name'])),
+                            DataCell(Text(fish['quantity'].toString())),
+                            DataCell(LinkWidget(fish['link'])),
+                          ]))
+                      .toList(),
+                ),
+              ),
+            if(widget.planningMode == 0 || widget.planningMode == 2)
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: DataTable(
+                  columnSpacing: 10,
+                  dataRowMaxHeight: 50,
+                  columns: [
+                    DataColumn(
+                      label: const SizedBox(
+                        width: 130, // gleiche Breite für alle Spalten
+                        child: Text('Pflanzenart'),
+                      ),
+                      onSort: (int columnIndex, bool ascending) => {},
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 160, // gleiche Breite für alle Spalten
+                        child: Text('Gestaltungstyp'),
+                      ),
+                    ),
+                    const DataColumn(
+                      label: SizedBox(
+                        width: 50, // gleiche Breite für alle Spalten
+                        child: Text(''),
+                      ),
+                    ),
+                  ],
+                  rows: data['plants']
+                      .map<DataRow>((plant) => DataRow(cells: [
+                            DataCell(Text(plant['plant_name'])),
+                            DataCell(Text(plant['plant_type'].toString())),
+                            DataCell(LinkWidget(plant['link'])),
+                          ]))
+                      .toList(),
+                ),
+              ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.9,
+              ),
+              child: Text(data['reason']),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LinkWidget extends StatelessWidget {
+  final String url;
+
+  LinkWidget(this.url);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: const Icon(Icons.shopping_cart, color: Colors.lightGreen),
+      onTap: () => launchURL(url),
     );
   }
 
-  Widget buildSection(String title, Map<String, dynamic> sectionData) {
-    List<Widget> tiles = [Text(title, style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))];
-    sectionData.forEach((key, value) {
-      if (value is Map) {
-        tiles.add(
-            ListTile(
-              title: Text(key),
-              subtitle: Text(value['name']),
-              trailing: IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () => launch('https://example.com/${value['name']}'),
-              ),
-            )
-        );
-      } else if (value is List) {
-        for (var item in value) {
-          tiles.add(
-              ListTile(
-                title: Text(key),
-                subtitle: Text('Test'),
-                trailing: IconButton(
-                  icon: Icon(Icons.shopping_cart),
-                  onPressed: () => launch('https://example.com/${item['name']}'),
-                ),
-              )
-          );
-        }
-      } else {
-        tiles.add(
-            ListTile(
-              title: Text(key),
-              subtitle: Text(value.toString()),
-              trailing: IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () => launch('https://example.com/$key'),
-              ),
-            )
-        );
-      }
-    });
-    return Card(child: Column(children: tiles));
+  void launchURL(String url) async {
+    // Hier solltest du den URL im Browser öffnen. In einem richtigen App würde man Flutter's url_launcher benutzen.
+    print('Öffne URL: $url');
   }
 }
